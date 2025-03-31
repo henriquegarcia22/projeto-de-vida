940bd4005ee1031d0c02c162c1127e810dc0160c
henriquegarcia22 <146238839+henriquegarcia22@users.noreply.github.com>
2025-03-31 11:53:29 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");{
 console.log(botoes);
-for ( let i = 0; i <botoes.length; i++){
-    console.log(i);
+}
+botoes [i].onclik = function (){
+    botoes[i].classList.add("ativo");
 }
